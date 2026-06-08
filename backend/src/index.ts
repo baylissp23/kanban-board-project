617b90875f063ba20013178ea96bd2863f35c6a2
phb1g24 <phb1g24@soton.ac.uk>
2026-06-08 16:14:57 +0100

    fix: fix issue where tickets were not being fetched correctly from Mongo at app start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 app.get("/api/tickets", async (req, res) => {
     try {
         const tickets = await TicketModel.find({});
-        res.json();
+        res.json(tickets);
     } catch (err) {
         res.status(500).json({ message : "Failed to fetch tickets" });
     }
